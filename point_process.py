Fixed problem with computation of densities of point processes, when beta or gamma equal to zero. It raised an error, since I stored log of its values.

point_process.py
@@ -7,16 +7,16 @@ def num_of_neighbors(config, R):
 def total_num_of_neighbors(config, R):
     return np.sum(num_of_neighbors(config, R)) // 2
 
-def saturated_interaction_statistic(config, R, saturated_parameter):
+def saturated_interaction_statistic(config, R, saturation_parameter):
     neighbors = num_of_neighbors(config, R)
-    return np.sum(np.minimum(neighbors, saturated_parameter))
+    return np.sum(np.minimum(neighbors, saturation_parameter))
 
 class StraussDensity:
 
   def __init__(self, R, beta, gamma):
     self.R = R
-    self.log_beta = np.log(beta)
-    self.log_gamma = np.log(gamma)
+    self.log_beta = np.log(beta) if beta>0 else None
+    self.log_gamma = np.log(gamma) if gamma>0 else None
 
   def _interaction_statistic(self, config):
     return total_num_of_neighbors(config, self.R)
@@ -26,15 +26,23 @@ class StraussDensity:
     Return density, calculated up to a constant, which is usually unknown
     '''
     n = len(config)
+    if self.log_beta is None:
+        return 1 if n == 0 else 0
 
-    return np.exp(self.log_beta * n + self.log_gamma * self._interaction_statistic(config))
+    res = np.exp(self.log_beta * n)
+    inter = self._interaction_statistic(config)
+
+    if self.log_gamma is None:
+        return 0 if inter > 0 else res
+
+    return res * np.exp(self.log_gamma * inter)
 
 class SaturatedDensity:
 
     def __init__(self, R, beta, gamma, saturation):
         self.R = R
-        self.log_beta = np.log(beta)
-        self.log_gamma = np.log(gamma)
+        self.log_beta = np.log(beta) if beta > 0 else None
+        self.log_gamma = np.log(gamma) if gamma > 0 else None
         self.saturation = saturation
 
     def _interaction_statistic(self, config):
@@ -42,17 +50,22 @@ class SaturatedDensity:
 
     def __call__(self, config):
         n = len(config)
-        first_order_log = self.log_beta * n
-        second_order_log = self.log_gamma * self._interaction_statistic(config)
-        return np.exp(first_order_log + second_order_log)
+        if self.log_beta is None:
+            return 1 if len(config) == 0 else 0
+        res = np.exp(self.log_beta * n)
+        inter = self._interaction_statistic(config)
+        if self.log_gamma is None:
+            return res if inter==0 else 0
+        second_order_log = self.log_gamma * inter
+        return res * np.exp(second_order_log)
 
 if __name__ == "__main__":
     R = 1.1
     beta = 0.1
-    gamma = 0.9
-    saturation = 1
-    strauss = SaturatedDensity(R, beta, gamma, saturation)
+    gamma = 0
+    # saturation = 1
+    strauss = StraussDensity(R, beta, gamma)
     config = np.array([[0, 0], [1, 0], [0, 1]])
     print(strauss(np.empty(shape=(0, 2))))
-    print(strauss._interaction_statistic(config))
+    # print(strauss._interaction_statistic(config))
     print(strauss(config))